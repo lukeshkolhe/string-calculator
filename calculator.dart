8961f98552af20866dca377dd554e3eb31e10b97
lukesh <lukesh.kolhe@stage.in>
2025-07-23 01:48:29 +0530

    added handling for explicit delimiters and spliting numbers according to the same

--- calculator.dart
+++ calculator.dart
@@ -4,9 +4,26 @@ class Calculator {
     if (numbers.isEmpty) {
       return 0;
     }
+
+    // if the string starts with //, the delimiter is the character after the //
+    final String? delimiter;
+    if (numbers.startsWith('//')) {
+      // extract the delimiter
+      delimiter = numbers.substring(2, 3);
+
+      // remove the delimiter and the new line from the numbers string
+      numbers = numbers.substring(4);
+    } else {
+      delimiter = null;
+    }
     
-    // split the string by commas and new lines using regexand parse numbers
-    List<int> numberList = numbers.split(RegExp(r'[,\n]')).map(int.parse).toList();
+    // split the string by commas and new lines or the delimiter using regex and parse numbers
+    List<int> numberList;
+    if (delimiter != null) {
+      numberList = numbers.split(RegExp('[,\\n${RegExp.escape(delimiter)}]')).map(int.parse).toList();
+    } else {
+      numberList = numbers.split(RegExp(r'[,\n]')).map(int.parse).toList();
+    }
 
     int sum = 0;
 
